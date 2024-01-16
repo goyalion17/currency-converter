a96ae9404cfa9a0d5ce3aa97f482685144b664e1
Irina Saveleva <goyalion17@gmail.com>
2024-01-16 18:59:46 +0100

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
-function App() {
+// `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
+
+export default function App() {
+  // const [output, setOutput] = useState("");
+  const [amount, setAmount] = useState("");
+
+  useEffect(function () {
+    async function fetchCurrencies() {
+      const res = await fetch(
+        `https://api.frankfurter.app/latest?amount=${amount}&from=EUR&to=USD`
+      );
+      const data = await res.json();
+      console.log(data);
+    }
+    fetchCurrencies();
+  }, [amount]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        type="text"
+        value={amount}
+        onChange={(e) => setAmount(Number(e.target.value))}
+      />
+      <select>
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+      <select>
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+      <p>output</p>
     </div>
   );
 }
-
-export default App;
