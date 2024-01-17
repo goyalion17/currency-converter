0de6bac9880d545b4314fd35a7d6f310833e1103
Irina Saveleva <goyalion17@gmail.com>
2024-01-17 17:06:06 +0100

    useEffect, useState, controlled selections and input, isLoading, disabled

--- src/App.js
+++ src/App.js
@@ -1,42 +1,61 @@
 import { useEffect, useState } from "react";
 
-// `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
-
 export default function App() {
-  // const [output, setOutput] = useState("");
-  const [amount, setAmount] = useState("");
+  const [output, setOutput] = useState("");
+  const [amount, setAmount] = useState(1);
+  const [baseCurrency, setBaseCurrency] = useState("EUR");
+  const [quoteCurrency, setQuoteCurrency] = useState("USD");
+  const [isLoading, setIsLoading] = useState(false);
 
-  useEffect(function () {
-    async function fetchCurrencies() {
-      const res = await fetch(
-        `https://api.frankfurter.app/latest?amount=${amount}&from=EUR&to=USD`
-      );
-      const data = await res.json();
-      console.log(data);
-    }
-    fetchCurrencies();
-  }, [amount]);
+  useEffect(
+    function () {
+      async function fetchCurrencies() {
+        setIsLoading(true);
+        const res = await fetch(
+          `https://api.frankfurter.app/latest?amount=${amount}&from=${baseCurrency}&to=${quoteCurrency}`
+        );
+        const data = await res.json();
+        console.log(data);
+        setOutput(data.rates[quoteCurrency].toFixed(2));
+        setIsLoading(false);
+      }
+      if (baseCurrency === quoteCurrency) return setOutput(amount);
+      fetchCurrencies();
+    },
+    [amount, baseCurrency, quoteCurrency]
+  );
 
   return (
     <div>
       <input
-        type="text"
+        type="number"
         value={amount}
         onChange={(e) => setAmount(Number(e.target.value))}
+        /* disabled={isLoading} */
       />
-      <select>
+      <select
+        value={baseCurrency}
+        onChange={(e) => setBaseCurrency(e.target.value)}
+        disabled={isLoading}
+      >
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
         <option value="INR">INR</option>
       </select>
-      <select>
+      <select
+        value={quoteCurrency}
+        onChange={(e) => setQuoteCurrency(e.target.value)}
+        disabled={isLoading}
+      >
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
         <option value="INR">INR</option>
       </select>
-      <p>output</p>
+      <p>
+        {output} {quoteCurrency}
+      </p>
     </div>
   );
 }
